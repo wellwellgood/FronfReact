9c9255973e04301bbce35d5f4dadff670075478a
WellWellGood <wellwellgood@example.com>
2025-05-16 17:01:51 +0900

    1

--- src/serverF/DB.js
+++ src/serverF/DB.js
@@ -1,17 +1,13 @@
-const mariadb = require("mariadb");
-const dotenv = require('dotenv');
-dotenv.config();
+const mariadb = require('mariadb');
+require('dotenv').config();
 
 const pool = mariadb.createPool({
   host: process.env.MARIADB_HOST,
-  port: Number(process.env.MARIADB_PORT),
+  port: process.env.MARIADB_PORT,
   user: process.env.MARIADB_USER,
   password: process.env.MARIADB_PASSWORD,
   database: process.env.MARIADB_NAME,
   connectionLimit: 5,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 module.exports = pool;
