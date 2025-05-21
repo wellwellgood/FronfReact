e6d5f4c67566d4b87d5f7a01712c8071d6edd6ae
WellWellGood <wellwellgood@example.com>
2025-05-21 11:04:34 +0900

    1

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const pool = require("../DB.js");
+const pool = require("../DB.js").default;
 
 const router = express.Router();
 
